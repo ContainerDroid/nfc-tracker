46eac1f6ef17007bd6d7e47abe7a176d73de4360
Ioana Ciornei <ciorneiioana@gmail.com>
2018-01-24 20:52:49 +0200

    change MainActivity to AppNoActionBar theme
    Signed-off-by: Ioana Ciornei <ciorneiioana@gmail.com>

--- app/src/main/java/com/eden/apps/nfctracker/MainActivity.java
+++ app/src/main/java/com/eden/apps/nfctracker/MainActivity.java
@@ -3,8 +3,6 @@ package com.eden.apps.nfctracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.lineageplus.apps.nfctracker.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
