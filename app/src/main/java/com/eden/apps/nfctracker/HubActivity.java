56cb4f00e4879b8ba3faae9c66a18b5a3a181dfd
Vladimir Oltean <olteanv@gmail.com>
2018-01-25 22:42:42 +0200

    HubActivity: initial commit (basic BottomNavigation activity)
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

---
+++ app/src/main/java/com/eden/apps/nfctracker/HubActivity.java
@@ -0,0 +1,44 @@
+package com.eden.apps.nfctracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HubActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.hub_action_readers:
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.hub_action_enclosures:
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.hub_action_categories:
+                    //mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/eden/apps/nfctracker/MainActivity.java
+++ app/src/main/java/com/eden/apps/nfctracker/MainActivity.java
@@ -28,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mHubDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent = new Intent(getApplicationContext(), HubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
