b8c40ada843cf0483fe82c4d87f2d4e6cdfd0dac
Vladimir Oltean <olteanv@gmail.com>
2018-01-27 01:06:37 +0200

    hub: initial commit for RecyclerView + GridLayoutManager
    * kudos to https://stackoverflow.com/questions/40587168/simple-android-grid-example-using-recyclerview-with-gridlayoutmanager-like-the
      and https://stackoverflow.com/questions/34278892/implement-a-recyclerview-in-a-fragment
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- app/src/main/java/com/eden/apps/nfctracker/HubCategoriesFragment.java
+++ app/src/main/java/com/eden/apps/nfctracker/HubCategoriesFragment.java
@@ -3,6 +3,10 @@ package com.eden.apps.nfctracker;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +17,28 @@ import android.view.ViewGroup;
  */
 public class HubCategoriesFragment extends Fragment {
 
+    HubCategoriesAdapter mAdapter;
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
 
     public HubCategoriesFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hub_categories, container, false);
+        View view = inflater.inflate(R.layout.fragment_hub_categories, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.hub_categories_recyclerview);
+        mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
+
+        mAdapter = new HubCategoriesAdapter(getActivity(), data);
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
     }
 
 }
