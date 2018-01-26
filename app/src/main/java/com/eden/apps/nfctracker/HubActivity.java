0411c214427ea44cea3d223d08d2fabe9aa7a374
Vladimir Oltean <olteanv@gmail.com>
2018-01-26 02:08:06 +0200

    hub: initial preferences fragment for readers
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- app/src/main/java/com/eden/apps/nfctracker/HubActivity.java
+++ app/src/main/java/com/eden/apps/nfctracker/HubActivity.java
@@ -4,41 +4,51 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HubActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.hub_action_readers:
-                    //mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.hub_action_enclosures:
-                    //mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.hub_action_categories:
-                    //mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    Toolbar mToolbar = null;
+    BottomNavigationView mBottomNavigationView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // setup toolbar
+        mToolbar = findViewById(R.id.hub_toolbar);
+        mToolbar.setTitle(getResources().getText(R.string.hub_title_status));
+        setSupportActionBar(mToolbar);
+
+        // set bottom navigation view
+        mBottomNavigationView = findViewById(R.id.hub_bottom_navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.hub_action_readers:
+                        item.setChecked(true);
+                        return true;
+                    case R.id.hub_action_enclosures:
+                        item.setChecked(true);
+                        return true;
+                    case R.id.hub_action_categories:
+                        item.setChecked(true);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        // set status page as the default one
+        mBottomNavigationView.getMenu().findItem(R.id.hub_action_readers).setChecked(true);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.hub_fragment_placeholder, new HubReadersFragment())
+                .commit();
     }
 
 }
---
+++ app/src/main/java/com/eden/apps/nfctracker/HubReadersFragment.java
@@ -0,0 +1,27 @@
+package com.eden.apps.nfctracker;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HubReadersFragment extends PreferenceFragment {
+
+    public HubReadersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.hub_readers_preferences);
+    }
+}
