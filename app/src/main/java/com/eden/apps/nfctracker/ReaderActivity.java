0f1d9d9efd3cf8e6cd0c8a31165a8865f4a49d87
Vladimir Oltean <olteanv@gmail.com>
2018-01-27 03:10:28 +0200

    ReaderActivity: implement reading of NFC tag ID by tech filter
    * kudos to https://stackoverflow.com/questions/37684119/intent-chooser-is-shown-with-nfc-tech-discovered-filter-and-foreground-dispatch
      and https://gist.github.com/luixal/5768921
      and https://stackoverflow.com/questions/7486958/problem-with-nfc-intent-dispatch
      and https://code.tutsplus.com/tutorials/reading-nfc-tags-with-android--mobile-17278
      and https://www.sitepoint.com/learn-android-nfc-basics-building-a-simple-messenger/
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- app/src/main/java/com/eden/apps/nfctracker/ReaderActivity.java
+++ app/src/main/java/com/eden/apps/nfctracker/ReaderActivity.java
@@ -1,18 +1,45 @@
 package com.eden.apps.nfctracker;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcBarcode;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 
 public class ReaderActivity extends AppCompatActivity {
 
     Toolbar mToolbar = null;
     BottomNavigationView mBottomNavigationView = null;
+    public NfcAdapter mNfcAdapter;
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +74,10 @@ public class ReaderActivity extends AppCompatActivity {
 
         // set readers setting page as the default one
         mSwitchFragment(mBottomNavigationView.getMenu().findItem(R.id.reader_action_settings), new ReaderSettingsFragment());
+
+        // NFC setup
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        handleIntent(getIntent());
     }
 
 
@@ -56,5 +87,98 @@ public class ReaderActivity extends AppCompatActivity {
                 .replace(R.id.reader_fragment_placeholder, fragment)
                 .commit();
         return true;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        Log.d(TAG, "handleIntent: outside if");
+        Log.d(TAG, intent.toString());
+        if (intent.getAction() == NfcAdapter.ACTION_TECH_DISCOVERED) {
+            Log.d(TAG, "handleIntent: inside if");
+            Toast.makeText(getApplicationContext(), "NFC Tag: " +
+                            this.ByteArrayToHexString(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)),
+                            Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Converting byte[] to hex string:
+    private String ByteArrayToHexString(byte [] inarray) {
+        int i, j, in;
+        String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        String out= "";
+        for (j = 0; j < inarray.length; j++) {
+            in = (int) inarray[j] & 0xff;
+            i = (in >> 4) & 0x0f;
+            out += hex[i];
+            i = in & 0x0f;
+            out += hex[i] + ":";
+        }
+        return out.substring(0, out.length() - 1);
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[] {
+                new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
+        };
+        String[][] techList = new String[][] {
+                new String[] { NfcA.class.getName() },
+                new String[] { NfcB.class.getName() },
+                new String[] { NfcF.class.getName() },
+                new String[] { NfcV.class.getName() },
+                new String[] { NfcBarcode.class.getName() },
+                new String[] { MifareClassic.class.getName() },
+                new String[] { MifareUltralight.class.getName() },
+        };
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
     }
 }
--- app/src/main/java/com/eden/apps/nfctracker/ReaderStatusFragment.java
+++ app/src/main/java/com/eden/apps/nfctracker/ReaderStatusFragment.java
@@ -2,10 +2,15 @@ package com.eden.apps.nfctracker;
 
 
 import android.app.Fragment;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 public class ReaderStatusFragment extends Fragment {
@@ -18,7 +23,18 @@ public class ReaderStatusFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reader_status, container, false);
+        View mView = inflater.inflate(R.layout.fragment_reader_status, container, false);
+        TextView mReaderStatusText = (TextView) mView.findViewById(R.id.reader_status_text);
+        NfcAdapter mNfcAdapter = ((ReaderActivity) getActivity()).mNfcAdapter;
+
+        if (mNfcAdapter == null) {
+            mReaderStatusText.setText(R.string.reader_status_no_nfc);
+        } else if (!mNfcAdapter.isEnabled()) {
+            mReaderStatusText.setText(R.string.reader_status_nfc_not_enabled);
+        } else {
+            mReaderStatusText.setText(R.string.reader_status_all_good);
+        }
+        return mView;
     }
 
 }
